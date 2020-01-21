9de47ed60b9ccdf20f2a134d7eb738fbe3c8ec9a
matt <matt@atlasagency.com.au>
2020-01-22 09:19:35 +1100

    first

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/*
+ ** TailwindCSS Configuration File
+ **
+ ** Docs: https://tailwindcss.com/docs/configuration
+ ** Default: https://github.com/tailwindcss/tailwindcss/blob/master/stubs/defaultConfig.stub.js
+ */
+module.exports = {
+  theme: {
+    colors: {
+      black: "#222",
+      white: "#fff",
+      teal: "#333",
+      pink: "#333"
+    }
+  },
+  variants: {},
+  plugins: []
+};
